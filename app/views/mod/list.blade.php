e592d81f7727f0e83a0093ba9a6b1a57d08ed811
Philip Lawall <philiplawall@gmail.com>
2015-03-03 08:49:14 +0100

    modified list.blade

--- app/views/mod/list.blade.php
+++ app/views/mod/list.blade.php
@@ -48,7 +48,12 @@
 <script type="text/javascript">
 $(document).ready(function() {
     $('#dataTables').dataTable({
-        "order": [[ 1, "asc" ]]
+        "order": [[ 1, "asc" ]],
+        aLengthMenu: [
+            [25, 50, 100, 200, -1],
+            [25, 50, 100, 200, "All"]
+        ],
+        iDisplayLength: -1
     });
 
 });
